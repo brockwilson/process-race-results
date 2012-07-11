409507a2020ced6375005a6726f2b0b5cdf5e757
Brock Wilson <brock.wilson@gmail.com>
2012-07-11 16:18:43 -0700

    processed through to csv

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject process-race-results "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clojure-csv/clojure-csv "2.0.0-alpha1"]]
+  :plugins [[lein-swank "1.4.4"]
+            ])
--- src/process_race_results/core.clj
+++ src/process_race_results/core.clj
@@ -1,5 +1,35 @@
 (ns process-race-results.core
-  (:use [clojure.string :only [split-lines]]))
+  (:use [clojure.string :only [split-lines]])
+  (:use [clojure.set :only [intersection]])
+  (:use [clojure-csv.core :only [write-csv]]))
 
-(defn return-columns [filename]
-  (clojure.string/split-lines (slurp filename)))
+
+
+(defn split-file-to-rows [file-name]
+  (clojure.string/split-lines (slurp file-name)))
+
+(defn find-char-in-string [string char]
+  (map first
+       (filter #(= (second %) char)
+               (map-indexed vector string))))
+
+(defn find-common-whitespace [list-of-strings]
+  (sort (seq (reduce clojure.set/intersection
+                     (map (fn [x] (set (find-char-in-string x \space)))
+                          list-of-strings)))))
+
+(defn find-data-indexes [list-of-strings]
+  (let [common-white-space (find-common-whitespace list-of-strings)]
+    (for [[x y] (apply map vector [common-white-space (rest common-white-space)])
+	 :when (> (- y x) 1)]
+	[x y])))
+
+(defn split-line-by-indexes-list [line indexes-list]
+  (for [indexes indexes-list]
+    (subs line (first indexes) (second indexes))))
+
+(defn do-everything [file-name]
+  (let [file-as-list-of-strings (split-file-to-rows file-name)
+        data-indexes (find-data-indexes file-as-list-of-strings)
+        split-data (map #(split-line-by-indexes-list % data-indexes) file-as-list-of-strings)]
+    (clojure-csv.core/write-csv split-data)))
