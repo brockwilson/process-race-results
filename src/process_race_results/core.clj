5703fe2054fe39317ca2cdfacc2d6a885269bbe4
Brock Wilson <brock.wilson@gmail.com>
2012-08-07 17:19:20 -0700

    Satisfactory Sinister 7 data processing

--- src/process_race_results/core.clj
+++ src/process_race_results/core.clj
@@ -1,14 +1,60 @@
 (ns process-race-results.core
-  (:use [clojure.string :only [split-lines split trim]])
+  (:use [clojure.string :only [split-lines split trim lower-case join]])
   (:use [clojure.set :only [intersection]])
-  (:use [clojure-csv.core :only [write-csv]])
-  (:use [clojure.math.numeric-tower :only [expt]])
+  (:use [clojure-csv.core ])
+  (:use [clojure.math.numeric-tower :only [expt floor]])
   (:use (incanter core stats charts)))
 
 
+(defn split-line-by-indexes-list [line indexes-list]
+  (for [indexes indexes-list]
+    (clojure.string/trim (subs line (first indexes) (second indexes)))))
 
-(defn split-file-to-rows [file-name]
-  (clojure.string/split-lines (slurp file-name)))
+(defn convert-to-time-in-seconds [string]
+  (if (not (= (.indexOf string ":") -1))
+    (let [split-time (clojure.string/split string #":")]
+      (try (reduce +
+                   (map-indexed (fn [index value]
+                                  (* (expt 60 index) (Integer/parseInt value)))
+                                (reverse split-time)))
+           (catch NumberFormatException e
+             string)))
+    string))
+
+(defn recursive-convert-time-in-seconds-to-string [time-in-seconds exponent accumulator]
+  (if (= exponent -1)
+    accumulator
+    (let [time-in-exponent (floor (/ time-in-seconds
+                                     (expt 60 exponent)))]
+      (recur (- time-in-seconds (* time-in-exponent
+                                   (expt 60 exponent)))
+             (- exponent 1)
+             (str accumulator
+                  (if (= "" accumulator)
+                    ""
+                    ":")
+                  (if (< time-in-exponent 10)
+                    (str "0" time-in-exponent)
+                    (str time-in-exponent)))))))
+
+(defn convert-time-in-seconds-to-string [time-in-seconds]
+  (recursive-convert-time-in-seconds-to-string time-in-seconds 2 ""))
+
+       
+
+
+
+       (let [time-in-exponent (floor (/ time-in-seconds
+                                   (expt 60 exponent)))
+        base-string (str time-in-exponent)]
+    (if (< exponent 0)
+      nil
+      (str base-string
+           ":"
+           (recur (- time-in-seconds (* time-in-exponent (expt 60 exponent)))
+                  (- exponent 1))))))
+          
+  
 
 (defn find-char-in-string [string char]
   (map first
@@ -26,36 +72,70 @@
 	 :when (> (- y x) 1)]
 	[x y])))
 
-(defn split-line-by-indexes-list [line indexes-list]
-  (for [indexes indexes-list]
-    (clojure.string/trim (subs line (first indexes) (second indexes)))))
+(defn process-rows-by-common-whitespace [rows]
+  (let [data-indexes (find-data-indexes rows)]
+    (map #(split-line-by-indexes-list % data-indexes) rows)))
 
-(defn convert-to-time-in-seconds [string]
-  (if (not (= (.indexOf string ":") -1))
-    (let [split-time (clojure.string/split string #":")]
-      (reduce +
-                      (map-indexed (fn [index value]
-                                     (* (expt 60 index) (Integer/parseInt value)))
-                                   (reverse split-time))))
-    string))
+(defn process-file-by-whitespace [file-name]
+  (process-rows-by-common-whitespace (clojure.string/split-lines (slurp file-name))))
 
-(defn process-file-name [file-name]
-  (let [file-as-list-of-strings (split-file-to-rows file-name)
-        data-indexes (find-data-indexes file-as-list-of-strings)
-        split-data (map (fn [string]
-                          (let [asdf (split-line-by-indexes-list string data-indexes)]
-                            (map convert-to-time-in-seconds asdf)))
-                        file-as-list-of-strings)]
+(defn process-file-by-delimiter [file-name delimiter]
+  (let [data (clojure-csv.core/parse-csv (slurp file-name) :delimiter delimiter)]
+    (map (fn [row] (map (fn [row-entry] (convert-to-time-in-seconds row-entry)) row)) data)))
+
+;; data structures come out slightly different in each case.
+(defn process-file [file-name delimiter]
+    (if (= delimiter :whitespace)
+      (process-file-by-whitespace file-name)
+      (process-file-by-delimiter file-name delimiter)))
+
+
+
+
     
-    split-data))
 
-(defn process-file-name-to-csv [file-name]
-  (let [data (process-file-name file-name)
-        csv-string (clojure-csv.core/write-csv (map (fn [x]
-                                                      (map str x))
-                                                    data))
-        csv-filename (str file-name ".csv")]
-    (spit csv-filename csv-string)))
+
+  
+(defn calculate-percentile [datum data]
+  (/ (.indexOf (sort data)
+               datum)
+     (length data)
+     1.0))
+
+
+;; Sinister 7 specific functions
+
+(defn extract-single-leg-datum [row leg-number]
+  (try (- (nth row (+ leg-number 4))
+          (if (= leg-number 1)
+            0
+            (nth row (+ leg-number 3))))
+       (catch ClassCastException e
+         false)))
+
+(defn extract-single-leg-data [data leg-number]
+  (let [proc-func #(extract-single-leg-datum % leg-number)
+        filtered-data (filter proc-func data)]
+    (map proc-func filtered-data)))
+
+(defn process-sinister-7-results [file-name team-name]
+  (let [data (process-file file-name \tab)
+        team-details (first (filter (fn [x]
+                                      (= (clojure.string/lower-case team-name)
+                                         (clojure.string/lower-case (nth x 2))))
+                                    data))]
+    (map (fn [x] (let [leg-data (extract-single-leg-data data x)]
+                   (view (histogram (map #(/ % 3600.0) leg-data)
+                                    :nbins 20
+                                    :x-label "Time (hr)"
+                                    :y-label "Counts"
+                                    :title (format "Leg %d" x)))
+                   [:leg x
+                    :time (convert-time-in-seconds-to-string (extract-single-leg-datum team-details x))
+                    :percentile (* 100
+                                   (calculate-percentile (extract-single-leg-datum team-details x)
+                                                         leg-data))]))
+         (range 1 8))))
 
 ;; (let [data (process-file-name "tender-knee-2012.dat")]
 ;; 			       (view (histogram (map (fn [x] (nth x 3)) data))))
